c67ebfed3389df18e372b4366ce7b6e0bbc831b8
Wahed Koudsi <82679451+Wahed-koudsi2001@users.noreply.github.com>
2022-05-06 06:05:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let weather = {
+    'apikey': "85ea5954e285b5777ff212728efa41c7",
+    fetchWeather: function (city) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikey}`)
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main
+        const { speed } = data.wind;
+        document.querySelector('.city').innerText = `Weather in ${name}`;[]
+        document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector('.description').innerText = description;
+        document.querySelector('.temp').innerText = `${temp} °C`;
+        document.querySelector('.humidity').innerText = `humidity: ${humidity}%`;
+        document.querySelector('.wind').innerText = `Wind speed: ${speed} km/h`;
+        document.querySelector('.weather').classList.remove('loading');
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector('.search-bar').value);
+    },
+};
+
+document.querySelector('.search button').addEventListener('click', function () {
+    weather.search();
+});
+
+document.querySelector('.search-bar').addEventListener('keyup', function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+
+weather.fetchWeather('Aleppo');
